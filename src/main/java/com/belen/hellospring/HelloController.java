2c98891b1bcaa568adaa8e614483a06022d07eed
BelenG26 <belen.aguilar@go.stcloudstate.edu>
2025-12-02 00:29:31 -0600

    Hello Spring Boot app

--- src/main/java/com/belen/hellospring/HelloController.java
+++ src/main/java/com/belen/hellospring/HelloController.java
@@ -1,4 +1,25 @@
 package com.belen.hellospring;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller //telling Spring this class responds to user request
 public class HelloController {
+    @GetMapping("/") //homepage
+    public String greeting(Model model) { //combined w/ templates like hashmap
+
+        String name = "BA";//value
+        model.addAttribute("name", name);
+
+        return "hello";//all request start w/ 127.0.0.1 //returns the hello,mustache method
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+
 }
+
